24adda79fa2e973389cd3dae26f9cdb663e7326c
yd2960 <yd2960@nyu.edu>
2025-04-06 12:53:20 -0400

    changed port to 8000 since 5000 is used on macs

--- web-app/app.py
+++ web-app/app.py
@@ -80,4 +80,4 @@ def upload():
 
 if __name__ == '__main__':
     os.makedirs(UPLOADS, exist_ok=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
