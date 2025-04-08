e7a5ce17f0dba9e1eea7428da1af39c22746a219
alw9411 <alw9411@nyu.edu>
2025-04-08 14:40:44 -0400

    add db functionality to app.py

--- web-app/db.py
+++ web-app/db.py
@@ -1,17 +1,26 @@
 import pymongo
+from bson.objectid import ObjectId
+import datetime
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# USERNAME = os.environ.get("USERNAME")
+# PASSWORD = os.environ.get("PASSWORD")
+# HOST_NAME = os.environ.get("HOST_NAME")
+# PORT = os.environ.get("PORT", 27017)
 
 connection = pymongo.MongoClient("mongodb://localhost:27017/")
 
-db = connection["epic_metal_machine"]
+db = connection["epic-metal-machine"]
 
 """
-image_to_convert = {
-    image: String,
-    transcription: String,
+
+file = {
+    text: String,
+    timestamp: Datetime
 }
+
 
 """
 
